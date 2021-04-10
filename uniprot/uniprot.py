b938bf38f370ace070e022eeddbb897ae241e46f
J-E-J-S <james.sanders1711@gmail.com>
2021-04-10 13:03:14 +0100

    error handling in api request

--- uniprot/uniprot.py
+++ uniprot/uniprot.py
@@ -6,12 +6,32 @@ def uniprot_request(name, organism, review='yes'):
 
     base = 'https://www.uniprot.org/uniprot/'
     query = 'name: "{name}" AND taxonomy: {organism} AND reviewed:{reviewed}'.format(name=name, organism=organism, reviewed=review)
-    payload = {'query': query,'format': 'fasta', 'limit':'1'} # Gets fasta sequence limited to 1 sequence 
+    payload = {'query': query,'format': 'fasta', 'limit':'1'} # Gets fasta sequence limited to 1 sequence
 
     r = requests.get(base, params=payload)
-    print(r.text)
+
+    if r.status_code == 200:
+        if len(r.text) == 0:
+            return click.echo('Query Does Not Exist.') # Uniprot will not always return a 404 code if protein doesn't exist
+        else:
+            return r.text # Return .fasta sequence 
+
+    # Error Handling
+    if r.status_code == 400:
+        return click.echo('Problem with Input.')
+    if r.status_code == 404:
+        return click.echo('Query Does Not Exist.')
+    if r.status_code == 500 or r.status_code == 503:
+        return click.echo('Uniprot API Service Down.')
+    elif r.status_code != 200:
+        return click.echo('An Error has Occurred.')
 
 
 
 
-uniprot_request('eEF1A', 's. cerevisiae')
+
+
+
+
+a = uniprot_request('eEF1A', 's. cerevisiae')
+print(a)
