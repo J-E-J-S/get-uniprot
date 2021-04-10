641ee2c3413c6ac71418367c650223b587bd648d
J-E-J-S <james.sanders1711@gmail.com>
2021-04-10 13:15:01 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+VERSION = '1.0'
+DESCRIPTION = 'Python CLI for accessing protein .fasta sequences using the UNIPROT API.'
+LONG_DESCRIPTION = 'This package contains a CLI that returns a proteins fasta sequence to the shell.'
+
+# Setting up
+setup(
+        name="get-uniprot",
+        version=VERSION,
+        author="James Sanders",
+        author_email="james.sanders1711@gmail.com",
+        url = 'https://github.com/J-E-J-S/get-uniprot',
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[
+            'requests==2.20.0',
+            'click==7.1.2'
+        ],
+        entry_points = {
+            'console_scripts':['uniprot=getUniprot.uniprot:cli']
+        }
+)
