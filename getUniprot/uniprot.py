89cd7c21bdf1a9effb88a150c89df662fee66497
J-E-J-S <james.sanders1711@gmail.com>
2021-04-11 15:48:48 +0100

    Added output to file fn

--- getUniprot/uniprot.py
+++ getUniprot/uniprot.py
@@ -1,14 +1,14 @@
 import requests
 import click
-import os
 
 @click.command()
 @click.option('-q', '--query', type=str, help='Name of protein.')
 @click.option('-o', '--organism', type=str, help='Name of organism.')
-def cli(query, organism, review='yes'):
+@click.option('-f', '--file', is_flag=True)
+def cli(query, organism, file):
 
     base = 'https://www.uniprot.org/uniprot/'
-    search = 'name: "{name}" AND taxonomy: {organism} AND reviewed:{reviewed}'.format(name=query, organism=organism, reviewed=review)
+    search = 'name: "{name}" AND taxonomy: {organism} AND reviewed:yes'.format(name=query, organism=organism)
     payload = {'query': search,'format': 'fasta', 'limit':'1'} # Gets fasta sequence limited to 1 sequence
 
     r = requests.get(base, params=payload)
@@ -17,7 +17,24 @@ def cli(query, organism, review='yes'):
         if len(r.text) == 0:
             return click.echo('Query Does Not Exist.') # Uniprot will not always return a 404 code if protein doesn't exist
         else:
-            return click.echo(r.text) # Output .fasta sequence to shell
+            # Output file if -f option raised
+            if file == True:
+                output_file = query + '_' + organism
+                output_file = output_file.replace(' ', '_') # Replace spaces
+                output_file = output_file.replace('.', '') # Replace periods
+                output_file = output_file + '.fasta' # Add extension after cleaning
+                try:
+                    f = open(output_file, 'x')
+                except:
+                    return click.echo('Error: File Already Exists.')
+
+                f.write(r.text) # Output sequence
+                f.close()
+
+                return click.echo(r.text) # Output to shell
+
+            else:
+                return click.echo(r.text) # Output .fasta sequence to shell
 
     # Error Handling
     if r.status_code == 400:
